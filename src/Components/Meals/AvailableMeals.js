af63d3183d4bcfbe30fe8b407ed9221f369cdfb7
rakeshdkd <rakeshd1012@gmail.com>
2023-09-19 16:36:57 +0530

    Added meals item list

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,19 +1,54 @@
-import React from 'react'
-import classes from './AvailableMeals.module.css'
+import React from "react";
+import classes from "./AvailableMeals.module.css";
+import Cards from "../UI/Cards";
 
-const dummy_meals=[]
+
+const dummy_meals = [
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
 
 const AvailableMeals = () => {
-    const mealsList =dummy_meals.map((meal)=><li>{meal.name}</li>)
+  const mealsList = dummy_meals.map((meal) => (
+    <li className={classes.meal}>
+      <div>
+        <h3>{meal.name}</h3>
+        <div className={classes.description}>{meal.description}</div>
+        <div className={classes.price}>${meal.price}</div>
+      </div>
+    </li>
+  ));
   return (
     <>
-<section className={classes.meals}>
-    <ul>
-        {mealsList}
-    </ul>
-</section>
+      <section className={classes.meals}>
+        <Cards>
+          <ul>{mealsList}</ul>
+        </Cards>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default AvailableMeals
+export default AvailableMeals;
